63ec5979bc504134ce370e1867ea63220383edd1
johngodoi <4JQbJf.sE[z>
2020-05-22 15:28:12 -0300

    Adding Spark SQL example

--- src/main/java/com/johngodoi/Main.java
+++ src/main/java/com/johngodoi/Main.java
@@ -4,6 +4,9 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
 import java.util.Arrays;
@@ -14,10 +17,23 @@ import static com.johngodoi.StringUtils.splitLinesOnWords;
 
 public class Main {
     public static void main(String[] args) {
+        sparkCoreExample(args[0]);
+        sparkSQLExample();
+    }
+
+    private static void sparkSQLExample() {
+        SparkSession sparkSession = SparkSession.builder().appName(Main.class.getName()).master("local[*]").enableHiveSupport().getOrCreate();
+        Dataset<Row> covid = sparkSession.sqlContext().read().format("csv").option("header", "true").load("src/main/resources");
+        covid.createOrReplaceTempView("covid");
+        sparkSession.sql("select count(distinct `Country/Region`) from covid").show();
+        sparkSession.close();
+    }
+
+    private static void sparkCoreExample(String arg) {
         SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount").setMaster("local[*]");
         JavaSparkContext ctx = new JavaSparkContext(sparkConf);
         ctx.setLogLevel("OFF");
-        JavaRDD<String> lines = ctx.textFile(args[0]);
+        JavaRDD<String> lines = ctx.textFile(arg);
 
         lines.cache();
         lines.take(10).forEach(IOUtils::println);
